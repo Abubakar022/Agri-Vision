12b92199521434db343a82f26115d966a33efed5
Abubakar022 <abubakar50008000@gmail.com>
2025-10-09 11:56:45 +0500

    Create OnBoarding Screen Page 3  with Urdu content and image layout

--- lib/src/presentation/screens/OnBoarding_Pages/page3.dart
+++ lib/src/presentation/screens/OnBoarding_Pages/page3.dart
@@ -1,4 +1,6 @@
+import 'package:agri_vision/src/presentation/space.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class page3 extends StatelessWidget {
   const page3({super.key});
@@ -8,6 +10,46 @@ class page3 extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     final height = size.height;
     final width = size.width;
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        width: width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Aspace.spaceVertical(height*0.1),
+           Text(
+                           "سمارٹ فارمنگ، ڈرون اسپرے کے ساتھ",
+                            textAlign: TextAlign.right,
+                            style: GoogleFonts.vazirmatn(
+                              color: Colors.black,
+                              fontWeight: FontWeight.normal,
+                              fontSize: width * 0.08,
+                              
+                            ),
+                          ),
+                          Aspace.spaceVertical(10),
+                           Text(
+                           "اب جدید انداز میں اسپرے کریں، ڈرون ٹیکنالوجی کے ساتھ وقت، لاگت اور محنت، سب میں بچت پائیں۔",
+                            textAlign: TextAlign.right,
+                            style: GoogleFonts.vazirmatn(
+                              color: Colors.black,
+                              fontWeight: FontWeight.normal,
+                              fontSize: width * 0.04,
+                              
+                            ),
+                          ),
+                          Aspace.spaceVertical(20),
+                           ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image.asset("assets/images/onboard3.png",height: height*0.4,width: width*0.9, alignment: Alignment.center,fit: BoxFit.cover,))
+           
+                        
+      
+        ],
+      ),
+      ),
+    );
   }
 }
